0c8493a7d9d3d62e5cec2e1554e512defd421b57
Jason Benaim <jkbenaim@gmail.com>
2020-09-15 23:43:34 -0700

    New license header

--- gbcamextract.c
+++ gbcamextract.c
@@ -1,25 +1,18 @@
 /*
- * gbcamextract.c, extracts photos from Game Boy Camera save files.
+ * Copyright (c) 2013-2020 jkbenaim et al.
  *
- * Copyright (c) 2013 jkbenaim. MIT license.
+ * This program is free software; you may redistribute and/or modify it under
+ * the terms of the expat license (also known as the "MIT license").
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * This program is distributed in the hope that it will be useful, but without
+ * any warranty; without even the implied warranty of merchantability or
+ * firness for a particular purpose.
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * For the full license text, see LICENSE.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ ******************************************************************************
+ *
+ * This file implements main() and all of the Game Boy-specific functions.
  *
  */
 
